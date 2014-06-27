1223b0a9917792e1e5d36f618d30643186ee8d46
GMcD <gary.macdonald@projectscapa.com>
2014-06-27 09:28:16 +0100

    Updated AppPreferences to use function prototype for easier integration.

--- www/AppPreferences.js
+++ www/AppPreferences.js
@@ -1,30 +1,32 @@
-define([
-	'cordova'
-], function() {
-       
-       var exec = cordova.require('cordova/exec');
+function AppPreferences() {
+}
 
-       var AppPreferences = {
+AppPreferences.prototype.get = function(key,success,fail) {
+    cordova.exec(success,fail,"AppPreferences","get",[key]);
+};
+AppPreferences.prototype.set = function(key,value,success,fail) {
+    cordova.exec(success,fail,"AppPreferences","set",[key, value]);
+};
+AppPreferences.prototype.load = function(success,fail) {
+    cordova.exec(success,fail,"AppPreferences","load",[]);
+};
+AppPreferences.prototype.show = function(activity,success,fail) {
+    cordova.exec(success,fail,"AppPreferences","show",[activity]);
+};
+AppPreferences.prototype.clear = function(success,fail) {
+    cordova.exec(success,fail,"AppPreferences","clear", []);
+};
+AppPreferences.prototype.remove = function(keyToRemove, success,fail) {
+    cordova.exec(success,fail,"AppPreferences","remove", [keyToRemove]);
+};
 
-            get: function(key,success,fail) {
-                 exec(success,fail,"AppPreferences","get",[key]);
-            },
-            set: function(key,value,success,fail) {
-                 exec(success,fail,"AppPreferences","set",[key, value]);
-            },
-            load: function(success,fail) {
-                 exec(success,fail,"AppPreferences","load",[]);
-            },
-            show: function(activity,success,fail) {
-                exec(success,fail,"AppPreferences","show",[activity]);
-            },
-            clear: function(success,fail) {
-                exec(success,fail,"AppPreferences","clear", []);
-            },
-            remove: function(keyToRemove, success,fail) {
-                exec(success,fail,"AppPreferences","remove", [keyToRemove]);
-            }
-        };
+AppPreferences.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
 
-       return AppPreferences;
-});
+  window.plugins.preferences = new AppPreferences();
+  return window.plugins.preferences;
+};
+
+cordova.addConstructor(AppPreferences.install);
